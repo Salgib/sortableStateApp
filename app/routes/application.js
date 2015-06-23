43c3eabb24efc77d7a88df844597d3072ecd10da
Jacob Smith <jacob@jacobsmith.io>
2015-06-23 11:20:27 -0400

    Implement product list
    1. `ember g route application`

---
+++ app/routes/application.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    return Ember.RSVP.hash({
+      comparison: [],
+
+      products: Ember.$.getJSON("products.json")
+    });
+  }
+});
