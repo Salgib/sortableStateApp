3ea8562cbae7b35e793dcecb209676d053d817af
Shawn Gibson <saljgib@gmail.com>
2015-06-24 09:17:05 -0400

    added sass

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,9 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return Ember.RSVP.hash({
-      comparison: [],
-
-      products: Ember.$.getJSON("products.json")
+      states: Ember.$.getJSON("states.json")
     });
   }
 });
